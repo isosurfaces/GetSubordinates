ce3e171ec5c6fb1e4bcccfead588dbbbba801a68
Brian Chang <btcyan@gmail.com>
2021-05-22 19:08:38 +1000

    Coding challenge -- BTC base

---
+++ Employees/Program.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Employees
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var currentWD = AppDomain.CurrentDomain.BaseDirectory;
+            var fileName = "userHeirarchy.json";
+            var output = "subordinates.json";
+            var defaultPath = Path.Combine(currentWD, fileName);
+            var outputPath = Path.Combine(currentWD, output);
+
+            // Load json file
+            // Default json input
+            Console.WriteLine($"Loading default user-role file: {defaultPath}");
+            Console.WriteLine("Please enter a file location or leave blank to use default: ");
+
+            // Custom json input
+            var path = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(path)) path = defaultPath;
+
+            var data = new UserRoleManager();
+            string errorMessage = null;
+
+            while (!data.LoadUserRoleFromFile(path, ref errorMessage))
+            {
+                Console.WriteLine($"Error: {errorMessage}");
+                Console.WriteLine("Please enter a file location or leave blank to use default: ");
+                path = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(path)) path = defaultPath;
+            }
+
+            // Get SubOrdinates
+            while (true)
+            {
+                Console.WriteLine("Please enter an Employee ID: ");
+                if (int.TryParse(Console.ReadLine(), out var idValue))
+                {
+                    var result = data.GetSubOrdinates(idValue);
+                    
+                    if (result != null)
+                    {
+                        var ids = result.Select(x => x.Id);
+
+                        // Return SubOrdinates
+                        Console.WriteLine($"Found {result.Count} subordinates: {string.Join(", ", ids)}");
+                        Console.WriteLine($"Writing result to file '{output}'.");
+                        using (StreamWriter file = File.CreateText(outputPath))
+                        {
+                            JsonSerializer serializer = new JsonSerializer();
+                            serializer.Serialize(file, result);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Employee ID.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Employee ID.");
+                }
+            }
+        }
+    }
+}
---
+++ Employees/UserRoleManager.cs
@@ -0,0 +1,173 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Employees
+{
+    class UserRoleManager {
+        public UserRole UserRoles = new UserRole();
+        public List<User> Users { get => UserRoles.Users; }
+        public List<Role> Roles { get => UserRoles.Roles; }
+        private Dictionary<int, List<int>> RoleHierarchy = new Dictionary<int, List<int>>();
+        private Dictionary<int, List<int>> SubRoleIDCache = new Dictionary<int, List<int>>();
+
+        public bool LoadUserRoleFromFile(string path, ref string errorMessage)
+        {
+            // File validation
+
+            if (!File.Exists(path))
+            {
+                errorMessage = "Invalid file path.";
+                return false;
+            }
+
+            using (var file = File.OpenText(path))
+            {
+                var serializer = new JsonSerializer();
+                try
+                {
+                    UserRoles = (UserRole)serializer.Deserialize(file, typeof(UserRole));
+                }
+                catch (Exception ex)
+                {
+                    errorMessage = ex.Message;
+                    return false;
+                }
+            }
+
+            // Input data validation
+
+            if (Users.Count == 0 || Users.Any(x => x.Id == int.MinValue || string.IsNullOrEmpty(x.Name) || x.Role == int.MinValue) ||
+                Roles.Count == 0 || Roles.Any(x => x.Id == int.MinValue || string.IsNullOrEmpty(x.Name) || x.Parent == int.MinValue))
+            {
+                errorMessage = "User data, Role date, Id/Name/Role/Parent fields must not be empty.";
+                return false;
+            }
+
+            var idDuplicates = Users?.GroupBy(x => x.Id)
+              .Where(g => g.Count() > 1)
+              .Select(y => y.Key)
+              .ToList();
+            if (idDuplicates?.Count > 0)
+            {
+                errorMessage = $"Multiple users with ID(s) were found: {string.Join(",", idDuplicates)}.";
+                return false;
+            }
+
+            idDuplicates = Roles?.GroupBy(x => x.Id)
+              .Where(g => g.Count() > 1)
+              .Select(y => y.Key)
+              .ToList();
+            if (idDuplicates?.Count > 0)
+            {
+                errorMessage = $"Multiple roles with ID(s) were found: {string.Join(",", idDuplicates)}.";
+                return false;
+            }
+
+            // End of input data validation
+
+            // Initialise role structure
+            BuildRoleHierarchy();
+            return true;
+        }
+
+        private void BuildRoleHierarchy()
+        {
+            if (Roles == null || Roles.Count == 0) return;
+            var result = new Dictionary<int, List<int>>();
+
+            // Build a parent-children dictionary for each parent role
+            // This dictionary is used to compile uncached result
+            foreach (var r in Roles)
+            {
+                var parentId = r.Parent;
+                var childID = r.Id;
+
+                if (!result.ContainsKey(parentId))
+                {
+                    result.Add(parentId, new List<int>());
+                }
+                result[parentId].Add(childID);
+            }
+            
+            RoleHierarchy = result;
+        }
+
+        public List<User> GetSubOrdinates(int id)
+        {
+            var user = Users?.FirstOrDefault(u => u.Id == id);
+            if (user == null) return null;
+
+            var subRoleIDs = GetSubRoleIDs(user.Role);
+            if (subRoleIDs == null) return null;
+
+            return Users.Where(x => subRoleIDs.Contains(x.Role)).ToList();
+        }
+
+        public List<int> GetSubRoleIDs(int parentId)
+        {
+            // Get cached result from previous searches if available
+            if (SubRoleIDCache.ContainsKey(parentId))
+            {
+                return SubRoleIDCache[parentId];
+            }
+
+            var result = new List<int>();
+
+            if (!RoleHierarchy.ContainsKey(parentId))
+            {
+                // No Subordinates
+                SubRoleIDCache.Add(parentId, result);
+                return result;
+            }
+
+            // Given a list of parents, find all children
+            // Given a list of children, find all grand children
+            // Find one generation of children per Do loop
+            var parentIDs = new List<int>() { parentId };
+            do
+            {
+                // Get cached result from previous searches if available
+                var parentsCached = SubRoleIDCache.Where(x => parentIDs.Contains(x.Key));
+                var childrenIDsCached = parentsCached.SelectMany(x => x.Value).ToList();
+
+                // Compile uncached result from RoleHierarchy child-parent relationships
+                parentIDs = parentIDs.Except(parentsCached.Select(x => x.Key)).ToList();
+                var parentsUnCached = RoleHierarchy.Where(x => parentIDs.Contains(x.Key));
+                var childrenIDsUnCached = parentsUnCached.SelectMany(x => x.Value);
+
+                result.AddRange(childrenIDsCached.Union(childrenIDsUnCached));
+
+                // Do until there is no one left in the next generation
+                parentIDs = childrenIDsUnCached.ToList();
+            }
+            while (parentIDs.Count > 0);
+
+            // Add to cache
+            SubRoleIDCache.Add(parentId, result);
+            return result;
+        }
+    }
+
+    class UserRole
+    {
+        public List<User> Users = new List<User>();
+        public List<Role> Roles = new List<Role>();
+    }
+
+    class User
+    {
+        public int Id = int.MinValue;
+        public string Name;
+        public int Role = int.MinValue;
+    }
+
+    class Role
+    {
+        public int Id = int.MinValue;
+        public string Name;
+        public int Parent = int.MinValue;
+    }
+}
